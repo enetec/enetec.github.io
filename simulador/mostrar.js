b8fa4718d3b149eb2173d627b533a8fb362f9e0b
ENETEC <marketing@enetec.unb.br>
2018-05-23 20:44:51 -0300

    Add files via upload

--- simulador/mostrar.js
+++ simulador/mostrar.js
@@ -5,6 +5,8 @@ var Estado = window.localStorage.getItem('Estado')
 var consumo= window.localStorage.getItem('consumo')
 var tarifa= window.localStorage.getItem('tarifa')
 
+
+
 var inc = [0,0,0,0,0,0,0,0,0,0,0,0];
 
 var meses= ['jan', 'fev', 'mar', 'abr', 'maio', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
@@ -82,31 +84,29 @@ while(placa < 1){
 
   media = media/i;
 
-  placa = Math.floor(3.6*consumo/(media*0.5*30*2.01));
+  placa = Math.floor(1000*consumo/(260*8*30*0.5));
 console.log(placa);
   i++;
 }
-
 /*
-//não sei o que está de errado, mas o payback é entre 4 e 6 anos
   var inv = Math.floor(placa*36/1000) + 1
 
   var preço = 780*placa + 5000*inv ;
 
   console.log(inv);
   console.log(placa);
-//  var payback = 3.6*preço/(placa*media*0.5*16.67*tarifa*12);
+  var payback = 1000*preço/(12*0.6*0.5*8*30*placa*260);
+  payback= Math.floor(100* payback)/100
+
 */
 
-
 //coloca para o usuário ver
-var max=Math.max(...inc)*16.67/i;
 
 
 document.getElementById("texto-nome").innerHTML="Prezado(a) " + nome + ",";
 document.getElementById("texto-placas").innerHTML="Número de placas necessárias: "+ placa +
-" de "+ Math.floor(100*max)/100 + " watts. A área necessária é de "+ Math.floor(100*placa*2.01)/100 + " metros quadrados.";
-document.getElementById("texto-custo").innerHTML= " O tempo de retorno é entre 4 e 6 anos."
+" de  260 watts. A área necessária é de "+ Math.floor(100*placa*2.01)/100 + " metros quadrados.";
+document.getElementById("texto-custo").innerHTML= " O tempo de retorno é entre"+ "4" + "e "+ "6" + " anos."
 document.getElementById("div-simulador").style.display="block";
 
 }
